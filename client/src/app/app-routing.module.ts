a802a222f25073ba9e78cf983c2145db6cff7a7e
airdy <jalbot17@gmail.com>
2020-10-25 22:38:45 +0200

    angular component added. approximate layout added.

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {ChatViewComponent} from './chat-view/chat-view.component';
+
+const routes: Routes = [
+  {path: '', component: ChatViewComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
